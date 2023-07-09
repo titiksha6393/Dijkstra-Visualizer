31252322d8ac5b25028a539eada7839e24beebe1
Titiksha Gupta <83998883+titiksha6393@users.noreply.github.com>
2023-07-09 15:21:04 +0530

    Add files via upload

---
+++ Visualizer/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Visualizer from './Visualizer/Visualizer'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App">
+        <Visualizer></Visualizer>
+      </header>
+    </div>
+  );
+}
+
+export default App;
